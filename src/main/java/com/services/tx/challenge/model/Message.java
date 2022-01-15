514485f2fd2e33d4953f781cfeccf94a33e2bdf0
zlatkela <milos.zlatkovic@3ap.ch>
2022-01-15 08:58:58 +0100

    Created database connection

--- src/main/java/com/services/tx/challenge/model/Message.java
+++ src/main/java/com/services/tx/challenge/model/Message.java
@@ -1,7 +1,30 @@
 package com.services.tx.challenge.model;
 
+import java.time.Instant;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
 public class Message {
 
-//  TODO: implement class
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @CreatedDate
+  private Instant createdAt;
+
+  private String author;
+
+  private String text;
 
 }
